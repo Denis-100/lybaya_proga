9fe0c4dc72bea10865ae6d92ddfb2c108ff41801
Denis-100 <den.shulmin@mail.ru>
2026-02-18 11:16:09 +0300

    Добавил основное тело программы

---
+++ main.py
@@ -0,0 +1,17 @@
+while True:
+    try:
+        num1 = int(input("Enter a number: "))
+        choise = input("Выберите операцию: + - * /   (0) - Выход")
+        num2 = int(input("Enter a number: "))
+        if choise == "+":
+            pass
+        elif choise == "-":
+            pass
+        elif choise == "*":
+            pass
+        elif choise == "/":
+            pass
+        else:
+            print("Ошибка выбора операции:")
+    except ValueError as e:
+        print(f"Ошибка, вводите цифры")
